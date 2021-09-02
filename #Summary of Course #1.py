b870c6bbc7b3a01273a396beb643ccd7eac2a444
John Rivero <81208412+John-Rivero@users.noreply.github.com>
2021-09-02 10:32:26 -0500

    Update #Summary of Course #1.py

--- #Summary of Course #1.py
+++ #Summary of Course #1.py
@@ -20,7 +20,11 @@ b = int (a)
 question = input (b + 1)
 print (question)
 
-#Conditional Chapter #3
+#---------------------------------------------------------------------------------
+
+#**********Chapter 3**************
+
+#Conditional 
 question = input ("what is the capital of Texas? ")
 if question == "Austin":
     print ("Good job")
@@ -31,7 +35,8 @@ elif question == "austin":
 else:
     print ("wrong answer")
 
-#Try and Except Chapter #3
+
+#Try and Except 
 question1 = input ('2 + 2? ')
 try:
     answer = int (question1)
@@ -43,8 +48,11 @@ if answer > 1:
 else:
     print ("Wrong answer, please enter a numerical value")
 
+#---------------------------------------------------------------------------------
 
-#Store and Reuse Chapter #4
+#**********Chapter 4**************
+
+#Store and Reuse 
 question = input ("Hi, What is your name? ")
 
 def welcome ():
@@ -57,3 +65,161 @@ question2 = input ("What is your last name? ")
 print (question2)
 welcome ()
 
+#----------------------------------------------------------------------------------
+
+
+
+#*********Chapter 5***********
+
+
+#Repeated Steps 
+n = 10
+while n > 0:
+    print (n)
+    n = n -2 
+print ('Blastoff!')
+print (n)
+
+#An Infinite Loop 
+#n = 5
+#while n > 0:
+#    print ('Lather')
+#    print ('Rinse')
+#print ('Dry OFf')
+
+#No Loop 
+#n = 0
+#while n > 0: 
+#    print ('Lather')
+#    print ('Rinse')
+#print ('Dry Off')
+
+#Breaking Out of a Loop 
+while True:
+    line = input ('> ')
+    if line == 'done':
+        break
+    print (line)
+print ('Done')
+
+#Finishing an Iteration with Continue 
+while True:
+    line = input ('> ')
+    if line [0] == '#' :
+        continue
+    if line == 'done' :
+        break
+    print (line)
+print ('Done! ')
+
+#Simple Definite Loop
+for i in [5, 4, 3, 2, 1] :
+    print (i)
+print ('Blastoff')
+
+
+friends = ['John', 'Karlene']
+for friend in friends:
+    print ('Merry Christmas:', friend)
+print ('Done')
+
+#Counting in a loop
+zork = 0
+print ('Before', zork)
+for thing in [9, 41, 12, 3, 74, 15] :
+    zork = zork + 1
+    print (zork, thing)
+print ('After, zork')
+
+#Summing in a Loop
+zork = 0
+print ('Before', zork)
+for thing in [9, 41, 12, 3, 74, 15] :
+    zork = zork + thing
+    print (zork, thing)
+print ('After, zork')
+
+#Finding the Average in a Loop
+count = 0
+sum = 0
+print ('Before', count, sum)
+for value in [9, 41, 12, 3, 74, 15]:
+    count = count + 1
+    sum = sum + value
+    print (count, sum, value)
+print ('After', count, sum, sum/count)
+
+#Filtering in a Loop
+print ('Before')
+for value in [9, 41, 12, 3, 74, 15]:
+    if value > 20:
+        print ('Large number', value)
+    print ('After')
+
+
+#Searh Using a Boolean Varaiable
+found = False
+print ('Before', found)
+for value in [9, 41, 12, 3, 74, 15]:
+    if value == 3:
+        found = True
+    print (found, value)
+print ('After', found)
+
+#How to find the largest value
+largest_so_far = -1
+print ('Before', largest_so_far)
+for the_num in [9, 41, 12, 3, 74, 15]:
+    if the_num > largest_so_far:
+        largest_so_far = the_num
+    print (largest_so_far, the_num)
+print ("After", largest_so_far)
+
+
+#How to find the smallest value
+smallest = None
+print ('Before')
+for value in [9, 41, 12, 3, 74, 15]:
+    if smallest is None:
+        smallest = value
+    elif value < smallest:
+        smallest = value
+    print (smallest, value)
+print ('After', smallest)
+
+
+
+
+
+
+
+
+
+
+
+
+
+largest = None
+smallest = None
+
+while True:
+    
+    try:
+        num = input ("Enter a number: ")
+        if num == 'done':
+            break
+        n = int (num)
+        
+        if largest is None or n > largest:
+            largest = n
+            
+        elif smallest is None or n < smallest:
+            smallest = n
+
+    except:
+        print ("Invalid input")
+        continue
+
+
+print("Maximum is", largest)
+print("Minimum is", smallest)
