a077a447f9de048147e0b66f19b209ba712190e9
John Rivero <81208412+John-Rivero@users.noreply.github.com>
2021-11-26 17:14:33 -0600

    Update Project 12.1: Polygon Cryptocurrency API.py

--- Project 12.1: Polygon Cryptocurrency API.py
+++ Project 12.1: Polygon Cryptocurrency API.py
@@ -7,13 +7,23 @@ cert_none = ssl.create_default_context()
 cert_none.check_hostname = False
 cert_none.verify_mode = ssl.CERT_NONE
 
-service_url = "https://api.polygon.io/v3/reference/tickers?market=crypto&active=true&sort=ticker&order=asc&limit=10&apiKey=RZcc5XVgetAL61Sj5IQu1t06piGlJHtZ"
+#Polygon API
+service_url = "https://api.polygon.io/v3/reference/tickers?market=crypto&active=true&sort=ticker&order=asc&limit=100&apiKey=RZcc5XVgetAL61Sj5IQu1t06piGlJHtZ"
 html = urllib.request.urlopen (service_url, context=cert_none)
 html1 = html.read().decode()
 
 x = json.loads (html1)
+#print (json.dumps(x, indent=4))
+#print (len(x))
 
-#print ('User count: ', len(x))
-print(json.dumps(x, indent=4))
+#Selected information for the cryptocurrency
+y = (x['results'])
+for y1 in y:
+    print ("Cryptocurrency Ticker: ", y1['ticker'])
+    print ("Name: ", y1['name'])
+    print ("Base currency symbol: ", y1["base_currency_symbol"])
+    print ("Currency: ", y1['currency_symbol'])
+    print ("Actively Traded?", y1['active'])
+    print (' ')
 
-#not finished
+print (len(x))
